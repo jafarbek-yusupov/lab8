dbb0967d28b68d5f0b98609d0d5f743a989abaf0
j.yusupov <j.yusupov@newuu.uz>
2024-10-21 12:45:24 +0500

    upd

--- main.cpp
+++ main.cpp
@@ -9,11 +9,30 @@ int getType(int n){
     return c%2==0 ? 1:0 ;
 }
 
+int digits(int n){
+    int d = 0;
+    while(n!=0){ n/=10; d++; }
+    return d;
+}
+
+// PROBLEM 16
+
+bool isLychrel(int n){
+    int d = digits(n);
+    int m = 0;
+    while(n!=0){
+        m += n%10 * pow(10, d-1);
+        n/=10; d--;
+    }
+
+    //return m + n == isPal(m + n) ? true : false;
+}
 
 
 int main() {
-    int n;
-    cin >> n;
-//    cout << (getType(n) == 1 ? "Even digit int" : "Odd digit int")  << endl;
+    int m, n;
+    cin >> m >> n;
+    cout << isLychrel(n);
+    //cout << (getType(n) == 1 ? "Even digit int" : "Odd digit int")  << endl;
     return 0;
 }
